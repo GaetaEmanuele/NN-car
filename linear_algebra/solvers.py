f758ff66cb69e4db05dbdb31a58c5c35be5713f5
GaetaEmanuele <emanuele.gaeta@mail.polimi.i>
2025-03-14 09:22:56 +0000

    correction Cholesky and new patter of Linear solver

--- linear_algebra/solvers.py
+++ linear_algebra/solvers.py
@@ -2,7 +2,7 @@
 from .matrix import Matrix,Vector 
 from math import sqrt
 
-def LU(A: Matrix,b:Vector):
+def LU(A: Matrix):
     if A.square():    
         L = Matrix.eye(A.rows)
         U = A.copy()
@@ -12,43 +12,48 @@ def LU(A: Matrix,b:Vector):
                 L[i,j] = U[i,j]/U[j,j]
                 for k in range(j,n):
                     U[i,k] -= L[i,j] * U[j,k]
-        
-        if A.cols != b.length():
-            raise IndexError("Dimension not compatible")
-        else:
-            x = Vector(b.length(),False,None)
-            y = Vector(b.length(),False,None)
-            # Forward substitution (Ly = b)
-            y[0] = b[0]
-            for i in range(1, A.rows):
-                y[i] = b[i] - sum(L[i, k] * y[k] for k in range(0, i))
-
-            # Backward substitution (Ux = y)
-            x[-1] = y[-1] / U[-1, -1]
-            for i in range(A.rows - 2, -1, -1):  
-                x[i] = (y[i] - sum(U[i, k] * x[k] for k in range(i + 1, A.cols))) / U[i, i]
-
-            return x,L,U
+            return L,U
     else:
         raise TypeError('A must be a square matrix')
-def Cholesky(A: Matrix, b: Vector):
-    if A.square():
-        H = Matrix(A.rows, A.cols, None)
-    
-        # 1st element of the diagonal
-        H[0, 0] = sqrt(A[0, 0])
-    
+def Cholesky(A: Matrix):
+    if  A.square():   
+        H = Matrix(A.rows, A.cols)  # Initialization
+
         for i in range(A.rows):
-            # Diagonal element
-            H[i, i] = sqrt(A[i, i] - sum(H[i, k]**2 for k in range(0, i)))
-        
-            # element below the diagonal
-            for j in range(0, i):
-                H[i, j] = (A[i, j] - sum(H[i, k] * H[j, k] for k in range(0, j))) / H[j, j]
+            # diagonal element
+            H[i, i] = sqrt(A[i, i] - sum(H[i, k]**2 for k in range(i)))
+
+            for j in range(i + 1, A.rows):  # j > i for obatain the lower triangular matrix
+                H[j, i] = (A[j, i] - sum(H[j, k] * H[i, k] for k in range(i))) / H[i, i]
 
         return H
     else:
         raise TypeError("A must be a square matrix")
+def direct_solver(L:Matrix,U:Matrix,b:Vector):
+    x = Vector(b.length(),False,None)
+    y = Vector(b.length(),False,None)
+    # Forward substitution (Ly = b)
+    y[0] = b[0]
+    for i in range(1, L.rows):
+        y[i] = (b[i] - sum(L[i, k] * y[k] for k in range(0, i)))/ L[i, i]
 
-def solve(A: Matrix,b):
-    return LU(A,b)
+    # Backward substitution (Ux = y)
+    x[-1] = y[-1] / U[-1, -1]
+    for i in range(L.rows - 2, -1, -1):  
+        x[i] = (y[i] - sum(U[i, k] * x[k] for k in range(i + 1, L.cols))) / U[i, i]
+    
+    return x,L,U
+
+def solve(A: Matrix,b:Vector,Lower=None,Upper=None):
+    if A.cols != b.length():
+            raise IndexError("Dimension not compatible")
+    else:
+        if Lower is None and Upper is None :
+            H = Cholesky(A)
+            Ht = H.transpose()
+            return direct_solver(H,Ht,b)
+        else:
+            return direct_solver(Lower,Upper,b)
+
+
+    
